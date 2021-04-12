333b9591b9b5b0a5e1f6fdab9e7c676a26a96a1f
Hilda Espinosa Guerrero <heg11091@HE011091.ad.penoles.mx>
2021-04-11 23:44:31 -0500

    Include date of creation

--- Python_Project.py
+++ Python_Project.py
@@ -1,4 +1,5 @@
 # Python Project 
+# Date: Mon March 29th, 2021
 
 # In this project, you will use data provided by Motivate, a bike share system provider for many major cities in the United States, 
 # to uncover bike share usage patterns. You will compare the system usage between three large cities: Chicago, New York City, and Washington, DC.
